5fad9767c3f95cefe676893749697300353edb19
Heikki Mehtänen <heikki@mehtanen.fi>
2017-10-15 15:03:45 +0300

    Add psutil dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-install_requires = ['click']
+install_requires = ['click', 'psutil']
 scripts = ['bin/acdsc']
 package_dir = {'acdsc': 'acdsc'}
 packages = find_packages()
