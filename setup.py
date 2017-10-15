09ceeb0852f19847f0ba69afaf5ef70f182799ea
Heikki Mehtänen <heikki@mehtanen.fi>
2017-10-15 12:34:10 +0300

    Add setuptools setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+from setuptools import setup, find_packages
+
+install_requires = ['click']
+scripts = ['acdsc']
+
+setup(name='acdsc',
+      version='0.0.1alpha',
+      description='Assetto Corsa Dedicated Server Control',
+      url='http://TBD',
+      author='Heikki Mehtänen',
+      author_email='heikki@mehtanen.fi',
+      license='Beerware',
+      install_requires=install_requires,
+      scripts=scripts)
